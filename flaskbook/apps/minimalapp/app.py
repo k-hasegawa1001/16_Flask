21d64fd4d1596860d373949a0ac50ce3772d073c
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-07-04 10:19:59 +0900

    1.9

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+# render_templateを追加でimportする
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
@@ -14,3 +15,9 @@ def index():
 def hello(name):
     # Python 3.6から導入されたf-stringで文字列を定義
     return f"Hello, {name}!"
+
+# show_nameエンドポイントを作成する
+@app.route("/name/<name>")
+def show_name(name):
+    # 変数をテンプレートエンジンに渡す
+    return render_template("index.html", name=name)
