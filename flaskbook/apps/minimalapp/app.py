8e57369446eee6516cd380c005dfbb4680bcb5ca
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-07-04 10:31:16 +0900

    1.10

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,5 +1,5 @@
-# render_templateを追加でimportする
-from flask import Flask, render_template
+# url_forを追加でimportする
+from flask import Flask, render_template,url_for
 
 app=Flask(__name__)
 
@@ -21,3 +21,11 @@ def hello(name):
 def show_name(name):
     # 変数をテンプレートエンジンに渡す
     return render_template("index.html", name=name)
+
+with app.test_request_context():
+    # /
+    print(url_for("index"))
+    # /hello/world
+    print(url_for("hello-endpoint", name="world"))
+    # /name/ichiro?page=1
+    print(url_for("show_name", name="ichiro",page="1"))
