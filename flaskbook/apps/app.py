d17cdb281f900c70eb90b14cb11609b6356ff1de
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-07-07 11:19:29 +0900

    2.1

---
+++ flaskbook/apps/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# create_app関数を作成する
+def create_app():
+    # Flaskインスタンス生成
+    app = Flask(__name__)
+
+    # crudパッケージからviewsをimportする
+    from app.crud import views as crud_views
+
+    # register_blueprintを使いviewsのcrudをアプリへ登録する
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    return app
