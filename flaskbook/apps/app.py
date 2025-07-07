8d448af5b6be95d4b683ebb698d4db04caf10533
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-07-07 11:46:13 +0900

    2.7

--- flaskbook/apps/app.py
+++ flaskbook/apps/app.py
@@ -1,9 +1,27 @@
+from pathlib import Path
 from flask import Flask
+from flask_migrate import Migrate # type: ignore
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+
+# SQLAlchemyをインスタンス化する
+db = SQLAlchemy()
 
 # create_app関数を作成する
 def create_app():
     # Flaskインスタンス生成
     app = Flask(__name__)
+    # アプリのコンフィグ設定をする
+    app.config.from_mapping(
+        SECRET_KEY="2AZSMss3p5QPBcY2hBsJ",
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+
+    # SQLAlchemyとアプリを連携する
+    db.init_app(app)
+
+    # Migrateとアプリを連携する
+    Migrate(app,db)
 
     # crudパッケージからviewsをimportする
     from apps.crud import views as crud_views
