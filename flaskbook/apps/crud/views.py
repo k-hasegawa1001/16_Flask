39be43824fd07a3ade1cf964716ab92d938a4aaa
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-07-07 12:22:47 +0900

    2.12

--- flaskbook/apps/crud/views.py
+++ flaskbook/apps/crud/views.py
@@ -1,3 +1,8 @@
+# dbをimportする
+from apps.app import db
+# Userクラスをimportする
+from apps.crud.models import User
+
 from flask import Blueprint,render_template
 
 # Blueprintでcrudアプリを生成する
@@ -12,3 +17,8 @@ crud=Blueprint(
 @crud.route("/")
 def index():
     return render_template("crud/index.html")
+
+@crud.route("/sql")
+def sql():
+    db.session.query(User).all()
+    return "コンソールログを確認してください"
