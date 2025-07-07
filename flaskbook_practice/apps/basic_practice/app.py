9a67f071860265deffa73e49e1636a197c44ddfb
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-07-07 11:33:28 +0900

    create flaskbook_practice

---
+++ flaskbook_practice/apps/basic_practice/app.py
@@ -0,0 +1,50 @@
+from flask import Flask,redirect, render_template,url_for,request,flash
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    redirect(url_for("greeting1"))
+
+@app.route("/greeting1")
+def greeting1():
+    return "こんにちは"
+
+@app.route("/greeting2/<name>")
+def greeting2(name):
+    return f"こんにちは、{name}さん"
+
+@app.route("/double/<int:num>")
+def double(num):
+    return "%d" % (num*2)
+
+@app.route("/greeting3/<str>")
+def greeting3(str):
+    return render_template("greeting3.html", name=str)
+
+@app.route("/static1")
+def static1():
+    return render_template('static1.html')
+
+@app.route("/form_in1")
+def form_in1():
+    return render_template("form_in1.html")
+
+@app.route("/form_out1", methods=["post"])
+def form_out1():
+    comment = request.form["comment"]
+    return render_template("form_out1.html", comment=comment)
+
+@app.route("/form_in2")
+def form_in2():
+    return render_template("form_in2.html")
+
+@app.route("/form_out2", methods=["post"])
+def form_out2():
+    comment1 = request.form["comment1"]
+    comment2 = request.form["comment2"]
+    comment3 = request.form["comment3"]
+    flash(comment1)
+    flash(comment2)
+    flash(comment3)
+    return
