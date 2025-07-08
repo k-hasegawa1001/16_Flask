7522b5238db4dc1a95bab2881111ed810c7b69f4
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-07-08 12:14:02 +0900

    3.5

--- flaskbook/apps/app.py
+++ flaskbook/apps/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager # type: ignore
 from flask_migrate import Migrate # type: ignore
 from flask_sqlalchemy import SQLAlchemy # type: ignore
 from flask_wtf.csrf import CSRFProtect # type: ignore
@@ -6,6 +7,14 @@ from apps.config import config
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
+
+# LoginManagerをインスタンス化する
+login_manager = LoginManager()
+# login_view属性に未ログイン時にリダイレクトするエンドポイントを指定する
+login_manager.login_view = "auth.signup"
+# login_message属性にログイン後に表示するメッセージを指定する
+# ここでは何も表示しないよう空を指定する
+login_manager.login_message = ""
 
 def create_app(config_key):
     app = Flask(__name__)
@@ -16,6 +25,9 @@ def create_app(config_key):
     db.init_app(app)
 
     Migrate(app,db)
+
+    # login_managerをアプリケーションと連携する
+    login_manager.init_app(app)
 
     from apps.crud import views as crud_views
 
