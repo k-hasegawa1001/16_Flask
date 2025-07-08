da4cd95109d212954f7255d5d30d88147ba24c7f
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-07-08 10:00:03 +0900

    2.15

--- flaskbook/apps/crud/views.py
+++ flaskbook/apps/crud/views.py
@@ -1,9 +1,8 @@
-# dbをimportする
+from apps.crud.forms import UserForm
 from apps.app import db
-# Userクラスをimportする
 from apps.crud.models import User
 
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,redirect,url_for
 
 # Blueprintでcrudアプリを生成する
 crud=Blueprint(
@@ -18,7 +17,21 @@ crud=Blueprint(
 def index():
     return render_template("crud/index.html")
 
-@crud.route("/sql")
-def sql():
-    db.session.query(User).all()
-    return "コンソールログを確認してください"
+@crud.route("/users/new", methods=["get","post"])
+def create_user():
+    # UserFromをインスタンス化する
+    form = UserForm()
+    # フォームの値をバリデートする
+    if form.validate_on_submit():
+        # ユーザーを作成する
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        # ユーザーを追加してコミットする
+        db.session.add(user)
+        db.session.commit()
+        # ユーザーの一覧画面へリダイレクトする
+        return redirect(url_for("crud.users"))
+    return render_template("crud/create.html", form=form)
