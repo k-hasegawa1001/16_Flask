8c12700c18447a3d3240f9e3160b895be5097b57
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-07-08 13:52:12 +0900

    practice 2.2

--- flaskbook_practice2/apps/app1/views.py
+++ flaskbook_practice2/apps/app1/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 app1 = Blueprint(
     "app1",
@@ -10,3 +10,11 @@ app1 = Blueprint(
 @app1.route("/")
 def index():
     return "hello"
+
+@app1.route("/page1")
+def page1():
+    return render_template("page1.html")
+
+@app1.route("/page2")
+def page2():
+    return render_template("page2.html")
