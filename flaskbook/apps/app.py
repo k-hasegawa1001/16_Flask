2a63c2ac4e90190af603bd3b267bdd534b088ed2
k_hasegawa <ktq2490335@stu.o-hara.ac.jp>
2025-07-08 11:52:12 +0900

    3.1

--- flaskbook/apps/app.py
+++ flaskbook/apps/app.py
@@ -1,34 +1,30 @@
-from pathlib import Path
 from flask import Flask
 from flask_migrate import Migrate # type: ignore
 from flask_sqlalchemy import SQLAlchemy # type: ignore
 from flask_wtf.csrf import CSRFProtect # type: ignore
 from apps.config import config
 
-# SQLAlchemyをインスタンス化する
 db = SQLAlchemy()
-
 csrf = CSRFProtect()
 
-# create_app関数を作成する
 def create_app(config_key):
-    # Flaskインスタンス生成
     app = Flask(__name__)
-    # アプリのコンフィグ設定をする
     app.config.from_object(config[config_key])
 
     csrf.init_app(app)
 
-    # SQLAlchemyとアプリを連携する
     db.init_app(app)
 
-    # Migrateとアプリを連携する
     Migrate(app,db)
 
-    # crudパッケージからviewsをimportする
     from apps.crud import views as crud_views
 
-    # register_blueprintを使いviewsのcrudをアプリへ登録する
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    # これから作成するauthパッケージからviewsをimportする
+    from apps.auth import views as auth_views
+
+    # register_blueprintを使いviewsのauthをアプリへ登録する
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
 
     return app
